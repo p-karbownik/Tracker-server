771e0a8da41f721681cf998906dd966672cc07ca
HasuNoHana <zusant22@gmail.com>
2021-05-14 12:18:25 +0200

    refactor Website and Event dao pattern, add CRUD

--- src/main/java/pl/edu/pw/pik/pikactivitytrackerserver/model/Event.java
+++ src/main/java/pl/edu/pw/pik/pikactivitytrackerserver/model/Event.java
@@ -1,5 +1,7 @@
 package pl.edu.pw.pik.pikactivitytrackerserver.model;
 
+
+//TODO how to use hibernate for non sql?
 public class Event {
     private int id;
     private int site_id;
--- src/main/java/pl/edu/pw/pik/pikactivitytrackerserver/service/WebsiteService.java
+++ src/main/java/pl/edu/pw/pik/pikactivitytrackerserver/service/WebsiteService.java
@@ -3,13 +3,36 @@ package pl.edu.pw.pik.pikactivitytrackerserver.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.edu.pw.pik.pikactivitytrackerserver.DAO.Dao;
 import pl.edu.pw.pik.pikactivitytrackerserver.DAO.WebsiteDAO;
+import pl.edu.pw.pik.pikactivitytrackerserver.model.User;
+import pl.edu.pw.pik.pikactivitytrackerserver.model.Website;
+
+import java.util.Optional;
 
 //it means that this class contains buisness logic
 @Service
 public class WebsiteService {
 
-    @Autowired
-    private WebsiteDAO websiteDAO;
+    private static Dao<Website> jpaWebsideDao;
+
+    public static Website getWebsite(long id) {
+        Optional<Website> website = jpaWebsideDao.get(id);
+
+        return website.orElseGet(
+                () -> new Website("no-non-existing url", -1));
+    }
+
+    public static void updateWebsite(Website website, String[] params) {
+        jpaWebsideDao.update(website, params);
+    }
+
+    public static void saveWebsite(Website website) {
+        jpaWebsideDao.save(website);
+    }
+
+    public static void deleteWebsite(Website website) {
+        jpaWebsideDao.delete(website);
+    }
 
 }
