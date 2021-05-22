1edae7b9ce7e9a323b91c60d8e2f7c580318facf
HasuNoHana <zusant22@gmail.com>
2021-05-22 12:33:46 +0200

    register

--- src/main/java/pl/edu/pw/pik/pikactivitytrackerserver/DTO/UserDto.java
+++ src/main/java/pl/edu/pw/pik/pikactivitytrackerserver/DTO/UserDto.java
@@ -1,33 +1,15 @@
 package pl.edu.pw.pik.pikactivitytrackerserver.DTO;
 
-import javax.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
 
-    @NotNull
-    @NotEmpty
     private String login;
-
-    @NotNull
-    @NotEmpty
     private String password;
 
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-
-    // standard getters and setters
 }
--- src/main/java/pl/edu/pw/pik/pikactivitytrackerserver/controller/UserController.java
+++ src/main/java/pl/edu/pw/pik/pikactivitytrackerserver/controller/UserController.java
@@ -1,10 +1,21 @@
 package pl.edu.pw.pik.pikactivitytrackerserver.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import pl.edu.pw.pik.pikactivitytrackerserver.service.UserService;
 
 public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @GetMapping(path = "/register/{login}/{password}")
+    public boolean register(@PathVariable String login, @PathVariable String password) {
+        if(userService.loginExists(login)){
+            return false;
+        } else {
+            return userService.register(login, password);
+        }
+    }
 }
