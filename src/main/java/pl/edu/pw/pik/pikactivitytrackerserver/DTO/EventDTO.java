5c50e7483c8489aef9edebb084840807e77ede9a
Patryk Karbownik <293113@pw.edu.pl>
2021-05-22 08:36:54 +0200

    added EventDTO

--- src/main/java/pl/edu/pw/pik/pikactivitytrackerserver/DTO/EventDTO.java
+++ src/main/java/pl/edu/pw/pik/pikactivitytrackerserver/DTO/EventDTO.java
@@ -4,7 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.UUID;
+import java.text.SimpleDateFormat;
+import java.sql.Timestamp;
 
 @Data
 @NoArgsConstructor
@@ -12,5 +13,9 @@ import java.util.UUID;
 
 public class EventDTO
 {
-    private UUID websiteToken;
+    private int id;
+    private String websiteToken;
+    private Timestamp appearanceDate;
+    private String eventData;
+
 }
