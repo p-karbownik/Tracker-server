e72849a5676e5ce666aa4ae62aa6caafea76b635
Patryk Karbownik <293113@pw.edu.pl>
2021-05-23 12:14:16 +0200

    changes in dataSourceConfig

--- src/main/java/pl/edu/pw/pik/pikactivitytrackerserver/configurations/SqlDataSourceConfig.java
+++ src/main/java/pl/edu/pw/pik/pikactivitytrackerserver/configurations/SqlDataSourceConfig.java
@@ -1,10 +1,13 @@
 package pl.edu.pw.pik.pikactivitytrackerserver.configurations;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 import javax.sql.DataSource;
 
@@ -16,6 +19,18 @@ public class SqlDataSourceConfig {
     public DataSource getDataSource()
     {
         return DataSourceBuilder.create().build();
+    }
+
+    public @Bean
+    MongoClient mongoClient()
+    {
+        return MongoClients.create(System.getenv("MD_URI"));
+    }
+
+    public @Bean
+    MongoTemplate mongoTemplate()
+    {
+        return new MongoTemplate(mongoClient(), "pik");
     }
 
 }
